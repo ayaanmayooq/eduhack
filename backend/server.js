51d6186a89f5fb75b7e01d0cd37c9a45957b1a9d
Ketan Yeluri <e0603110@u.nus.edu>
2023-10-07 02:41:10 +0800

    Setup scripts

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", function (req, res) {
-    res.send({ "name": "Jane Doe" }) // Should be json format
-})
+  res.send({ name: "Jane Doe" }); // Should be json format
+});
 
-app.listen(3000, () => {
-    console.log("app listening on port 3000")
-})
+app.listen(6969, () => {
+  console.log("app listening on port 6969");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import React from "react"
+import React from "react";
 
 class App extends React.Component {
-    state = {
-        name: ""
-    }
+  state = {
+    name: "",
+  };
 
-    componentDidMount() {
-        fetch("http://localhost:3000")
-            .then(res => res.json())
-            .then(data => this.setState({ name: data.name }))
-    }
+  componentDidMount() {
+    fetch("http://localhost:6969")
+      .then((res) => res.json())
+      .then((data) => this.setState({ name: data.name }));
+  }
 
-    render() {
-        return (
-            <h1>Hello {this.state.name}!</h1>
-        )
-    }
+  render() {
+    return <h1>Fuck you {this.state.name}!</h1>;
+  }
 }
 
-export default App
+export default App;
