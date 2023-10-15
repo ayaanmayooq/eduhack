f7ac0e737c962f390b64d0a5191b93de6b6c0150
Ketan Yeluri <e0603110@u.nus.edu>
2023-10-15 15:07:42 +0800

    Finish

--- frontend/src/app/result/page.js
+++ frontend/src/app/result/page.js
@@ -1,12 +1,19 @@
+"use client";
+import { useEffect } from "react";
+
 export default function Result() {
+  let score = undefined;
+  useEffect(() => {
+    score = localStorage.getItem("score");
+  });
   return (
     <>
-      <button
+      <h3
         type="submit"
         className="mx-auto px-4 py-2 rounded-md w-32 h-12 my-10 border-solid border-2 text-purple border-purple hover:scale-105 hover:border-yellow hover:text-yellow hover:shadow-focus font-bold"
       >
-        Download
-      </button>
+        {score}
+      </h3>
     </>
   );
 }
